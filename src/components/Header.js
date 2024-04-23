a99ba00d8811789e6c29fee2cef89d42c124d3a6
hidanbeing <nd6162bv@gmail.com>
2024-04-23 16:41:39 +0900

    add google ad

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,6 +20,11 @@ const Mobile = ({ children }) => {
 function Header() {
   return (
     <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2590867190158667"
+        crossorigin="anonymous"
+      ></script>
       <Desktop>
         <HeaderDesktop />
       </Desktop>

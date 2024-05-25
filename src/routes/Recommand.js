d200e73e2eb0e78d79e24a5c2c444881a4e899d4
hidanbeing <nd6162bv@gmail.com>
2024-05-25 22:24:38 +0900

    modify network

---
+++ src/routes/Recommand.js
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Header from "../components/header/Header";
+function Recommand() {
+  return (
+    <>
+      <Header />
+      <div>recommand</div>
+      <Footer />;
+    </>
+  );
+}
+
+export default Recommand;
